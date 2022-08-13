0e10f80ae6c3f46a82d7bb0ab9229ed098cfbe49
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-13 13:42:47 -0300

    feat(structures): create MiamiClient structure

---
+++ src/structures/client.ts
@@ -0,0 +1,119 @@
+import fs from 'node:fs';
+import * as Discord from 'discord.js';
+
+import { join } from 'node:path';
+import { Command } from '@types';
+
+import config from '../config';
+
+import Logger from '@shared/utils/logger';
+
+/**
+ * Represents the main Miami client
+ * 
+ * @class @extends Discord.Client
+ * 
+ * @prop {config} config - The main client environment variables
+ * @prop {Array<Command>} commands - The commands array 
+ */
+export class MiamiClient extends Discord.Client {
+  private readonly logger: Logger;
+
+  config: typeof config;
+  commands: Command[];
+
+  /**
+   * Create a new MiamiClient instance
+   * 
+   * @constructs MiamiClient
+   */
+  constructor() {
+    const clientOptions: Discord.ClientOptions = {
+      allowedMentions: {
+        parse: [
+          'roles',
+          'users'
+        ],
+        repliedUser: true
+      },
+      intents: 33551,
+      presence: {
+        activities: [
+          {
+            name: 'Being built up',
+            type: Discord.ActivityType.Playing
+          }
+        ]
+      }
+    }
+
+    super(clientOptions);
+
+    this.logger = Logger.it(this.constructor.name);
+    
+    this.config = config;
+    this.commands = [];
+
+    this.loadEvents();
+    // this.loadCommands();
+  }
+
+  /**
+   * Load and start client events
+   * 
+   * @private @method
+   * 
+   * @param {String} path - The events folder path
+   * 
+   * @returns {void} void
+   */
+  private loadEvents(path: string = 'src/events'): void {
+    const categories: string[] = fs.readdirSync(path);
+
+    for (const category of categories) {
+      const events: string[] = fs.readdirSync(`${path}/${category}`);
+
+      for (const event of events) {
+        const Event = require(join(process.cwd(), `${path}/${category}/${event}`)).default;
+        const evt = new (Event)(this);
+
+        this.on(evt.name, evt.run);
+      }
+    }
+  }
+
+  /**
+   * Load and start client commands
+   * 
+   * @private @method
+   * 
+   * @param {String} path - The commands folder path
+   * 
+   * @returns {void} void
+   */
+  private loadCommands(path: string = 'src/commands'): void {
+    const categories: string[] = fs.readdirSync(path);
+
+    for (const category of categories) {
+      const commands: string[] = fs.readdirSync(`${path}/${category}`);
+
+      for (const command of commands) {
+        const Command = require(join(process.cwd(), `${path}/${category}/${command}`)).default;
+        const cmd = new (Command)(this);
+
+        this.commands.push(cmd);
+      }
+    }
+  }
+  
+  /**
+   * Log the client in
+   * 
+   * @public @method @async
+   * 
+   * @returns {Promise<String>} token - The token of the account used
+   */
+  public login(): Promise<string> {
+    return super.login(this.config.token);
+  }
+}
