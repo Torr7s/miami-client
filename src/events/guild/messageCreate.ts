937b1d979eeb32d59a87fa41fce8bb8d49c45ae2
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-15 10:54:13 -0300

    feat(events/guild): create messageCreate event

---
+++ src/events/guild/messageCreate.ts
@@ -0,0 +1,49 @@
+import { ChannelType, Message } from 'discord.js';
+
+import { MiamiClient } from '@structures/client';
+import { EventBase } from '@structures/event';
+
+/**
+ * Represents a MessageCreate client event
+ * 
+ * @class @extends EventBase
+ * @classdesc Emitted whenever a message is created
+ * 
+ * @prop {MiamiClient} client - The MiamiClient instance
+ */
+export default class MessageCreateEvent extends EventBase {
+  client: MiamiClient;
+
+  /**
+   * @constructs MessageCreateEvent
+   * 
+   * @param {MiamiClient} client - The MiamiClient instance 
+   */
+  constructor(client: MiamiClient) {
+    super(client, 'messageCreate');
+
+    this.client = client;
+  }
+
+  /**
+   * Triggered as soon as a message is created
+   * 
+   * @param {Message} message - The created message
+   * 
+   * @returns {Promise<Message<boolean> | void>} message | void 
+   */
+  run = async (message: Message): Promise<Message<boolean> | void> => {
+    const { author, channel } = message;
+
+    if (author.bot || channel.type === ChannelType.DM) return;
+
+    if (message.content.match(new RegExp(`^<@!?${this.client.user.id}>( |)$`))) {
+      if (message.channel.type === ChannelType.GuildText) {
+        return message.reply({
+          isMessage: true,
+          content: `Olá, ${message.author}.`
+        });
+      }
+    }
+  }
+}
