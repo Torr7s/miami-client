eb6cf220d85fd0df036738bbb99ca2c7ee9e014b
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-15 16:43:32 -0300

    feat(config): remove colors obj
    - rename devId prop

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,7 @@
 export default {
   token: process.env.DISCORD_CLIENT_TOKEN as string,
-  colors: {
-    BLACK: 2303786,
-    LIGHT_BLACK: 2895667,
-    GREY: 9807270,
-    LIGHT_GREY: 12370112,
-    DARK_GREY: 9936031,
-    DARKER_GREY: 8359053
-  },
+  avatarURL: process.env.DISCORD_CLIENT_AVATAR_URL as string,
   guildId: process.env.DISCORD_GUILD_ID as string,
-  devId: process.env.DISCORD_DEV_ID as string,
-  mongoURI: process.env.MONGO_URI
+  ownerId: process.env.DISCORD_DEV_ID as string,
+  mongoURI: process.env.MONGO_URI as string
 }
