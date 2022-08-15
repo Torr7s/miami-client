8e488a8a7fb3b2d4858d99be073b3f591b8410b8
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-15 10:49:50 -0300

    feat(events/ready): setup logger

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,16 +1,21 @@
 import { MiamiClient } from '@structures/client';
 import { EventBase } from '@structures/event';
 
+import Logger from '@shared/utils/logger';
+
 export default class ReadyEvent extends EventBase {
+  private readonly logger: Logger;
+
   client: MiamiClient;
 
   constructor(client: MiamiClient) {
     super(client, 'ready');
 
     this.client = client;
+    this.logger = Logger.it(this.constructor.name);
   }
 
   run = (): void => {
-    console.log('Client connected sucessfully');
+    this.logger.info('Client connected successfully');
   }
 }
