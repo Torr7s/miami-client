69c8665008a5e9f7b127c24988b414a78f2aec8d
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-15 19:46:46 -0300

    docs(builders): add jsdoc to builders

--- src/shared/builders/button.ts
+++ src/shared/builders/button.ts
@@ -2,6 +2,18 @@ import { APIMessageComponentEmoji, ButtonBuilder, ButtonStyle } from 'discord.js
 
 import { ButtonOptions } from '@types';
 
+/**
+ * Represents the main Button builder
+ * 
+ * @class @implements {ButtonOptions}
+ * 
+ * @prop {String} custom_id - The buttom custom id
+ * @prop {Boolean} [disabled] - Wether the button is disabled
+ * @prop {APIMessageComponentEmoji} [emoji] - The button emoji to be displayed
+ * @prop {String} label - The button label 
+ * @prop {ButtonStyle} style: The button style
+ *  
+ */
 export class Button implements ButtonOptions {
   custom_id: string;
   disabled?: boolean;
@@ -9,13 +21,30 @@ export class Button implements ButtonOptions {
   label: string;
   style: ButtonStyle;
 
-  constructor(buttonOptions: ButtonOptions) {
-    buttonOptions.disabled ??= false;
+  /**
+   * @constructs Button
+   * 
+   * @param {ButtonOptions} options - The options for this button
+   * @param {String} options.custom_id - The buttom custom id
+   * @param {Boolean} [options.disabled] - Wether the button is disabled
+   * @param {APIMessageComponentEmoji} [options.emoji] - The button emoji to be displayed
+   * @param {String} options.label - The button label 
+   * @param {ButtonStyle} options.style: The button style
+   */
+  constructor(options: ButtonOptions) {
+    options.disabled ??= false;
 
-    Object.assign(this, buttonOptions);
+    Object.assign(this, options);
   }
-  
-  build(): ButtonBuilder {
+
+  /**
+   * Build a button with its options
+   * 
+   * @method @public
+   * 
+   * @returns {ButtonBuilder} button - The builded button
+   */
+  public build(): ButtonBuilder {
     const button: ButtonBuilder = new ButtonBuilder()
       .setCustomId(this.custom_id)
       .setDisabled(this.disabled)
--- src/shared/builders/embed.ts
+++ src/shared/builders/embed.ts
@@ -4,7 +4,27 @@ import { EmbedOptions } from '@types';
 
 import config from '../../config';
 
+/**
+ * Represents the main Embed builder
+ * 
+ * @class @extends EmbedBuilder
+ */
 export class Embed extends EmbedBuilder {
+
+  /**
+   * @constructor 
+   * 
+   * @param {EmbedOptions} options - The embed options
+   * @param {EmbedAuthorOptions} [options.author] - The embed author options
+   * @param {Number} [options.color] - The embed color
+   * @param {String} [options.description] - The embed description
+   * @param {Array<APIEmbedField>} [options.fields] - The embed fields
+   * @param {EmbedFooterData} [options.footer] - The embed footer options
+   * @param {EmbedAssetData} [options.thumbnail] - The embed thumbnail
+   * @param {Number} [options.timestamp] - The embed timestamp
+   * @param {String} [options.title] - The embed title
+   * @param {String} [options.url] - The embed URL
+   */
   constructor(options: EmbedOptions) {    
     const data: EmbedData | APIEmbed = {
       color: 2895667,
