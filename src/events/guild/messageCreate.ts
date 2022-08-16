1455384409771e84532c9069f0f3736ca26fc079
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-16 16:45:12 -0300

    feat(events/message): find or create guild and user at mongo

--- src/events/guild/messageCreate.ts
+++ src/events/guild/messageCreate.ts
@@ -2,6 +2,9 @@ import { ChannelType, Message } from 'discord.js';
 
 import { EventBase, MiamiClient } from '@structures/index';
 
+import { UserSchema } from '@shared/database/models/user';
+import { GuildSchema } from '@shared/database/models/guild';
+
 /**
  * Represents a MessageCreate client event
  * 
@@ -34,7 +37,7 @@ export default class MessageCreateEvent extends EventBase {
    * @returns {Promise<Message<boolean> | void>} message | void 
    */
   async run(message: Message): Promise<Message<boolean> | void> {
-    const { author, channel } = message;
+    const { author, channel, guild, member } = message;
 
     if (author.bot || channel.type === ChannelType.DM) return;
 
@@ -45,6 +48,14 @@ export default class MessageCreateEvent extends EventBase {
           content: `Olá, ${message.author}.`
         });
       }
+    }
+
+    if (guild.available) {
+      let mongoUSER: UserSchema = await this.client.usersDb.findOne({ userId: author.id, guildId: guild.id });
+      let mongoGUILD: GuildSchema = await this.client.guildsDb.findOne({ guildId: guild.id, ownerId: guild.ownerId });
+
+      mongoUSER ??= await this.client.usersDb.create({ userId: author.id, guildId: guild.id });
+      mongoGUILD ??= await this.client.guildsDb.create({ guildId: guild.id, ownerId: guild.ownerId });
     }
   }
 }
