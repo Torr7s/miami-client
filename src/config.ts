f84b277409eeefdf8165fdd4c2ee46dd98631b95
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-18 12:27:22 -0300

    feat(config): create messariApiKey variable

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ export default {
   avatarURL: process.env.DISCORD_CLIENT_AVATAR_URL as string,
   guildId: process.env.DISCORD_GUILD_ID as string,
   ownerId: process.env.DISCORD_DEV_ID as string,
-  mongoURI: process.env.MONGO_URI as string
+  mongoURI: process.env.MONGO_URI as string,
+  messariApiKey: process.env.MESSARI_API_KEY as string
 }
