dc3525f6f6175c034abde3fd730455b235700511
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-18 20:56:07 -0300

    feat(events/message): remove database check

--- src/events/guild/messageCreate.ts
+++ src/events/guild/messageCreate.ts
@@ -2,9 +2,6 @@ import { ChannelType, Message } from 'discord.js';
 
 import { EventBase, MiamiClient } from '@structures/index';
 
-import { UserSchema } from '@shared/database/models/user';
-import { GuildSchema } from '@shared/database/models/guild';
-
 /**
  * Represents a MessageCreate client event
  * 
@@ -48,14 +45,6 @@ export default class MessageCreateEvent extends EventBase {
           content: `Olá, ${message.author}.`
         });
       }
-    }
-
-    if (guild.available) {
-      let mongoUSER: UserSchema = await this.client.usersDb.findOne({ userId: author.id, guildId: guild.id });
-      let mongoGUILD: GuildSchema = await this.client.guildsDb.findOne({ guildId: guild.id, ownerId: guild.ownerId });
-
-      mongoUSER ??= await this.client.usersDb.create({ userId: author.id, guildId: guild.id });
-      mongoGUILD ??= await this.client.guildsDb.create({ guildId: guild.id, ownerId: guild.ownerId });
     }
   }
 }
