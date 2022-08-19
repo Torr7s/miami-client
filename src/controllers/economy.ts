601b407a308afd41fb81d861677edeed91351d3d
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-19 11:25:26 -0300

    docs(controllers/economy): add jsdoc

--- src/controllers/economy.ts
+++ src/controllers/economy.ts
@@ -2,8 +2,28 @@ import { UserSchema } from '@shared/database/models/user';
 
 import { formatMs } from '@shared/utils/functions/time';
 
+type State = {
+  status: boolean,
+  time?: string
+}
+
+/**
+ * Represents a Economy controller
+ * 
+ * @class
+ * @classdesc Will handle transactions and everything related to the economy
+ */
 export class EconomyController {
-  dailyCheck(daily: number): { status: boolean, time?: string } {
+  /**
+   * Check if the user can receive daily bonus
+   * 
+   * @public @method
+   * 
+   * @param {Number} daily - The user daily time
+   *  
+   * @returns {State} state - The current daily state
+   */
+  dailyCheck(daily: number): State {
     const dailyInterval: number = 8.64e+7;
     const timeout: number = (Date.now() - daily);
 
@@ -21,6 +41,15 @@ export class EconomyController {
     }
   }
 
+  /**
+   * Retrieve daily bonus
+   * 
+   * @public @method
+   * 
+   * @param {UserSchema} mongoUSER - The database user
+   *  
+   * @returns {Object} coins - The value received 
+   */
   async dailyRetrieve(mongoUSER: UserSchema): Promise<{ coins: number }> {
     let coinsToReceive: number = 5000;
 
