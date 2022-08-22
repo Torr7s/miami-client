c78fcb8bcd255b769bd18a4e546ac3e8426dd6db
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-21 23:53:36 -0300

    feat(commands/crypto): add support for top assets list

--- src/commands/others/crypto.ts
+++ src/commands/others/crypto.ts
@@ -1,8 +1,19 @@
-import { ApplicationCommandOptionType, InteractionReplyOptions } from 'discord.js';
+import {
+  ActionRowBuilder,
+  ApplicationCommandOptionType,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  CollectedInteraction,
+  ComponentType,
+  Interaction,
+  InteractionCollector,
+  InteractionReplyOptions,
+} from 'discord.js';
 
 import { CommandBase, CommandContext, MiamiClient } from '@structures/index';
 
-import { MessariAssetMetrics } from '@types';
+import { MessariAllAssets, MessariAssetMetrics } from '@types';
 import { MessariAssetMetricsModel, messariRequest } from '@helpers/messari'
 
 import { Embed } from '@shared/builders/embed';
@@ -48,6 +59,17 @@ export default class CryptoCommand extends CommandBase {
     this.client = client;
   }
 
+  formatPercent(percent: number): string {
+    let result: string = String(percent);
+    const fixed: string = percent.toFixed(2);
+
+    if (result.startsWith('-')) result = `<:decline:1011088020302221423> \`${fixed}\``;
+    else if (result !== '0') result = `<:growth:1011087978522755162> \`${fixed}\``;
+    else result = `\`${fixed}\``
+
+    return result;
+  }
+
   /**
    * Used to handle the incoming interaction
    * 
@@ -55,9 +77,9 @@ export default class CryptoCommand extends CommandBase {
    * 
    * @param {CommandContext} ctx - The command context  
    * 
-   * @returns {InteractionReplyOptions} options - The given options for ctx
+   * @returns {InteractionReplyOptions | void} options - The given options for ctx
    */
-  async run(ctx: CommandContext): Promise<InteractionReplyOptions> {
+  async run(ctx: CommandContext): Promise<InteractionReplyOptions | void> {
     const option: string = ctx.interaction.options.getString('ativo', true);
 
     const asset: MessariAssetMetrics = await messariRequest<MessariAssetMetrics>(`v1/assets/${option}/metrics`);
@@ -74,24 +96,129 @@ export default class CryptoCommand extends CommandBase {
     const metrics: MessariAssetMetricsModel = MessariAssetMetricsModel.build(asset);
 
     const description: string[] = [
-      `<:crypt:1009844607124770906> Dados: `,
+      `» \`Dados\`: `,
       `ㅤ• Preço USD: \`${toCurrency(metrics.priceUsd)}\` (Alterou \`${metrics.percentChangeUsdLast24h.toFixed(2)}%\` em 24h)`,
       `ㅤ• Volume nas últimas 24h: ${format(metrics.volumeLast24h)}`,
       `ㅤ• Última transação em: ${metrics.lastTradeAt}`,
-      `<:crypt:1009844607124770906> Capitalização do mercado: `,
+      `» \`Capitalização do mercado\`: `,
       `ㅤ• Rank: ${metrics.rank}`,
       `ㅤ• Dominância: \`${metrics.marketCapDominancePercent.toFixed(2)}%\``,
       `ㅤ• Capital atual USD: \`${toCurrency(metrics.currentMarketCapUsd)}\``
     ];
 
-    const embed: Embed = new this.client.embed(ctx.user, {
+    const mainEmbed: Embed = new this.client.embed(ctx.user, {
       author: {
-        name: `[${metrics.symbol}] ${metrics.name} (${metrics.id})`,
-        iconURL: this.client.user.displayAvatarURL()
+        name: `
+          [${metrics.symbol}] ${metrics.name} (${metrics.id})
+        `
       },
       description: description.join('\n')
     });
 
-    return ctx.reply({ embeds: [embed] });
+    const row: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder<ButtonBuilder>()
+      .addComponents(
+        /* Go to the next page button */
+        new this.client.button({
+          custom_id: 'next',
+          emoji: {
+            name: 'next',
+            id: '1011087528612343838',
+            animated: false
+          },
+          label: 'Ativos',
+          style: ButtonStyle.Secondary
+        }).build(),
+        /* Go to the previous page button */
+        new this.client.button({
+          custom_id: 'previous',
+          emoji: {
+            name: 'previous',
+            id: '1011087460371017818',
+            animated: false
+          },
+          label: `${metrics.name}`,
+          style: ButtonStyle.Secondary,
+          disabled: true
+        }).build()
+      );
+
+    await ctx.reply({
+      embeds: [mainEmbed],
+      components: [
+        row
+      ]
+    });
+
+    const filter = (i: Interaction): boolean => i.user.id === ctx.user.id;
+
+    const collector: InteractionCollector<ButtonInteraction> = ctx.channel.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      filter
+    });
+
+    collector.on('collect', async (target: CollectedInteraction): Promise<void> => {
+      if (!target.deferred) await target.deferReply({ ephemeral: true });
+
+      switch (target.customId) {
+        case 'next':
+          row.components[0].setDisabled(true)
+          row.components[1].setDisabled(false);
+
+          const res: MessariAllAssets = await messariRequest<MessariAllAssets>('v2/assets');
+
+          const assetsArr = res.data.slice(0, 9);
+          const assetsObj = Object.entries(assetsArr)
+            .map(([, asset]) => asset)
+            .sort((x, y): number => y.metrics.market_data.price_usd - x.metrics.market_data.price_usd)
+
+          const description: string[] = assetsObj.map((asset, index) => {
+            const {
+              name,
+              metrics: {
+                market_data: {
+                  price_usd,
+                  percent_change_usd_last_24_hours
+                }
+              }
+            } = asset;
+
+            return (
+              `\`${index + 1}\`ﾠ-ﾠ**${name}**ﾠ-ﾠ\`${toCurrency(price_usd)}\` (${this.formatPercent(percent_change_usd_last_24_hours)}% em 24h)`
+            );
+          });
+
+          const embed: Embed = new this.client.embed(ctx.user, {
+            title: 'Posição | Ativo | Preço USD',
+            author: {
+              name: 'Top ativos no momento'
+            },
+            description: `${description.join('\n')}`
+          });
+
+          await ctx.interaction.editReply({
+            embeds: [embed],
+            components: [
+              row
+            ]
+          });
+
+          break;
+
+        case 'previous':
+          row.components[0].setDisabled(false)
+          row.components[1].setDisabled(true);
+          
+          await ctx.interaction.editReply({
+            embeds: [mainEmbed],
+            components: [
+              row
+            ]
+          });
+
+          break;
+
+        default: break;
+      }
+    })
   }
 }
