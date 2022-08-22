9f2b69dd4b2d91f223d2a09328538e3ea29164f8
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-22 11:32:42 -0300

    docs(messari/request): add jsdoc

--- src/helpers/messari/assets/request/main.ts
+++ src/helpers/messari/assets/request/main.ts
@@ -1,6 +1,14 @@
 import { request } from 'undici';
 import { ResponseData } from 'undici/types/dispatcher';
 
+/**
+ * Perform a request to Messari api
+ * 
+ * @param {String} endpoint - The messari api endpoint to reach
+ * @param {Record<string, any>} [queryParams] - The query params for its request
+ *  
+ * @returns {Promise<T>} T - The given interface for its typing 
+ */
 export const messariRequest = async <T>(endpoint: string, queryParams?: Record<string, any>): Promise<T> => {
   const res: Awaited<T> = await request(`https://data.messari.io/api/${endpoint}`, {
     method: 'GET',
