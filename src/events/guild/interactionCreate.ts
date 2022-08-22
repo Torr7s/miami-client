f0ff2b53fdaf5735d9a610a50354b7a9481ad1b9
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-22 19:34:30 -0300

    chore(events/interaction): remake cooldown check due to Map
    - facing long timestamp even using toFixed

--- src/events/guild/interactionCreate.ts
+++ src/events/guild/interactionCreate.ts
@@ -1,4 +1,4 @@
-import { Collection, Interaction, InteractionResponse } from 'discord.js';
+import { Interaction, InteractionResponse, Message } from 'discord.js';
 
 import { Command } from '@types';
 
@@ -42,9 +42,9 @@ export default class InteractionCreateEvent extends EventBase {
    * 
    * @param {Interaction} interaction - The created interaction
    * 
-   * @returns {Promise<InteractionResponse<boolean> | void>} interaction | void 
+   * @returns {Promise<InteractionResponse<boolean> | void>} interaction | message | void 
    */
-  async run(interaction: Interaction): Promise<InteractionResponse<boolean> | void> {
+  async run(interaction: Interaction): Promise<InteractionResponse | Message | void> {
     try {
       const { user, guild } = interaction;
 
@@ -94,40 +94,43 @@ export default class InteractionCreateEvent extends EventBase {
               }
             }
 
-            const context: CommandContext = new CommandContext(this.client, interaction);
-
             if (!this.client.cooldowns.has(command.name)) {
               this.client.cooldowns.set(
-                command.name, 
-                new Collection()
+                command.name,
+                new Map<string, number>()
               );
-            };
+            }
 
-            const now: number = Date.now();
+            const dateNow: number = Date.now();
+            const commandCooldown: number = command.cooldown * 1e3;
 
-            const timestamps: Collection<string, number> = this.client.cooldowns.get(command.name);
-            const cooldownAmount: number = (command.cooldown ?? 3) * 1000;
+            const clientCooldowns: Map<string, number> = this.client.cooldowns.get(command.name);
 
-            if (timestamps.has(user.id) && user.id !== this.client.config.ownerId) {
-              const expiresAt: number = (timestamps.get(user.id) as number) + cooldownAmount;
+            if (clientCooldowns && clientCooldowns.has(command.name)) {
+              const leftTime: number = (clientCooldowns.get(command.name) as number) * commandCooldown; 
+            
+              if (dateNow < leftTime) {
+                const remainingTime: number = (leftTime - dateNow) / 1e3;
 
-              if (now < expiresAt) {
-                const remainingTime: number = (expiresAt - now) / 1000;
+                if (!interaction.deferred) await interaction.deferReply({ ephemeral: true });
 
-                return interaction.reply({
-                  ephemeral: true,
-                  content: `Aguarde ${remainingTime.toFixed(1)} para usar o comando \`${command.name}\` novamente.`
+                return interaction.editReply({ 
+                  content: `
+                    Este comando poderá ser executado novamente em \`${remainingTime.toFixed(1)}s\`, aguarde.
+                  `
                 });
               }
             }
 
-            if (timestamps) {
-              timestamps.set(user.id, now);
+            if (clientCooldowns) {
+              clientCooldowns.set(command.name, dateNow);
 
               setTimeout((): void => {
-                timestamps.delete(user.id);
-              }, cooldownAmount);
+                clientCooldowns.set(command.name, dateNow);
+              }, commandCooldown);
             }
+
+            const context: CommandContext = new CommandContext(this.client, interaction);
 
             command.run(context);
           }
