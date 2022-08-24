2c28ccc4ce71aceb31795ecfb7a3791c4c957b34
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-24 14:26:38 -0300

    docs(handlers/request): add jsdoc

--- src/subCommands/github/handlers/request/main.ts
+++ src/subCommands/github/handlers/request/main.ts
@@ -1,6 +1,15 @@
 import { request } from 'undici'
 import { ResponseData } from 'undici/types/dispatcher';
 
+/**
+ * Perform a request to Github json api
+ * 
+ * @function
+ * 
+ * @param {String} endpoint - The github api endpoint to reach
+ *  
+ * @returns {Promise<T | null>} T - The output with the given typing
+ */
 export const githubRequestHandler = async <T>(endpoint: string): Promise<T | null> => {
   const res: Awaited<T> = await request(`https://api.github.com/${endpoint}`, {
     method: 'GET',
