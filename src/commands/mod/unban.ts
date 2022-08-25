a0e26f0cd532c9d487273c3a6f1e13db1dfbaa5e
Matheus Torres <torresmiranda.rj@gmail.com>
2022-08-25 17:06:53 -0300

    feat(cmds/mod): create unban command

---
+++ src/commands/mod/unban.ts
@@ -0,0 +1,89 @@
+import {
+  ApplicationCommandOptionType,
+  DMChannel,
+  PermissionFlagsBits,
+  User
+} from 'discord.js';
+
+import { CommandBase, CommandContext, MiamiClient } from '@structures/index';
+
+/**
+ * Represents a Ban slash command
+ * 
+ * @class @extends CommandBase
+ * 
+ * @prop {MiamiClient} client - The MiamiClient instance
+ */
+export default class UnbanCommand extends CommandBase {
+  client: MiamiClient;
+
+  /**
+   * @constructs UnbanCommand
+   * 
+   * @param {MiamiClient} client - The MiamiClient instance 
+   */
+  constructor(client: MiamiClient) {
+    super(client, {
+      name: 'desbanir',
+      description: 'Desbanir um usuário do servidor',
+      category: 'Mod',
+      options: [
+        {
+          name: 'id',
+          description: 'Id do usuário',
+          descriptionLocalizations: {
+            'en-US': 'Target user id'
+          },
+          type: ApplicationCommandOptionType.String,
+          required: true
+        }
+      ],
+      permissions: {
+        appPerms: [PermissionFlagsBits.BanMembers],
+        memberPerms: [PermissionFlagsBits.BanMembers]
+      }
+    });
+
+    this.client = client;
+  }
+
+  /**
+   * Handle the incoming interaction as a command
+   * 
+   * @public @method @async
+   * 
+   * @param {CommandContext} ctx - The command context  
+   * 
+   * @returns {Promise<void>} void
+   */
+  async run(ctx: CommandContext): Promise<void> {
+    const targetUserId: string = ctx.interaction.options.getString('id');
+    
+    const targetUser: User = this.client.users.cache.get(targetUserId);
+
+    try {
+      const dm: DMChannel = targetUser && await targetUser?.createDM();
+      await dm?.send({
+        content: `
+          Você foi desbanido do servidor \`${ctx.guild.name}\` por \`${ctx.user.tag}\`.
+        `
+      });
+
+    } catch (error) {
+      if (targetUser && targetUser.dmChannel) await targetUser.deleteDM();
+    }
+
+    ctx.guild.bans.remove(targetUserId).then((): void => {
+      ctx.reply({
+        content: `O usuário <@${targetUserId}> foi desbanido do servidor.`
+      });
+    }).catch((_: any): void => {
+      ctx.reply({
+        ephemeral: true,
+        content: `
+          Banimento desconhecido, o id \`${targetUserId}\` não foi encontrado.
+        `
+      });
+    });
+  }
+}
