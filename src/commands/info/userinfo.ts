9653137814b135159e2e15e2f4597462281ee6f3
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-13 12:51:21 -0300

    style: add missing semicolon && open code block for conditional

--- src/commands/info/userinfo.ts
+++ src/commands/info/userinfo.ts
@@ -12,11 +12,6 @@ import { formatTimestamp } from '@/src/shared/utils/functions';
 export default class UserinfoCommand extends CommandBase {
   client: MiamiClient;
 
-  /**
-   * @constructs UserinfoCommand
-   * 
-   * @param {MiamiClient} client - The MiamiClient instance 
-   */
   constructor(client: MiamiClient) {
     super(client, {
       name: 'usuário',
@@ -51,15 +46,6 @@ export default class UserinfoCommand extends CommandBase {
     return statuses[status];
   }
 
-  /**
-   * Handle the incoming interaction as a command
-   * 
-   * @public @method @async
-   * 
-   * @param {CommandContext} ctx - The command context  
-   * 
-   * @returns {Promise<InteractionReplyOptions>} options - The given options for ctx
-   */
   async run(ctx: CommandContext): Promise<InteractionReplyOptions> {
     const user: User = ctx.resolvedUsers[0];
 
@@ -67,7 +53,7 @@ export default class UserinfoCommand extends CommandBase {
       .setAuthor(`Informações de ${user.tag}`)
       .setDescription(`• Conta criada em ${formatTimestamp(user.createdAt, 'D')} (${formatTimestamp(user.createdAt, 'R')})`)
       .setThumbnail(`${user.displayAvatarURL()}`)
-      .addField(':id: ID', `${user.id}`)
+      .addField(':id: ID', `${user.id}`);
 
     const member: GuildMember = ctx.guild.members.cache.get(user.id);
 
@@ -79,12 +65,13 @@ export default class UserinfoCommand extends CommandBase {
 
     const { flags } = resolveFlags(user.flags);
 
-    if (flags) embed.addField(':triangular_flag_on_post: Flags', `${flags}`);
+    if (flags) {
+      embed.addField(
+        ':triangular_flag_on_post: Flags',
+        flags
+      );
+    }
 
-    return ctx.reply({
-      embeds: [
-        embed.build()
-      ]
-    });
+    return ctx.reply({ embeds: [embed.build()] });
   }
 }
