bfac3996d318e6e2f33b8cadfc18824266580fee
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-13 18:12:41 -0300

    feat: implement Event interface
    - use key of ClientEvents for better manageability

--- src/structures/event.ts
+++ src/structures/event.ts
@@ -1,10 +1,13 @@
+import { ClientEvents } from 'discord.js';
+import { Event } from '../typings';
+
 import MiamiClient from './client';
 
-export default class EventBase {
+export default class EventBase implements Event {
   client: MiamiClient;
-  name: string;
+  name: keyof ClientEvents;
 
-  constructor(client: MiamiClient, name: string) {
+  constructor(client: MiamiClient, name: keyof ClientEvents) {
     this.client = client;
     this.name = name;
   };
