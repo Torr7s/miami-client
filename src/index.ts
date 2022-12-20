8b0c310799430f8c7e2681f0737b76d760e40835
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-20 12:31:40 -0300

    chore: log app errors && use IIFE function to boot the client

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,21 @@ import './shared/utils/alias';
 
 import MiamiClient from './structures/client';
 
-const miamiClient: MiamiClient = new MiamiClient();
+import { Logger } from './shared/utils/logger';
 
-miamiClient.login();
+const logger: Logger = Logger.it('App');
+
+process.on('unhandledRejection', (reason: unknown): void => {
+  logger.error('An UNHANDLED PROMISE has been found: ', reason);
+});
+
+process.on('uncaughtException', (error: Error): void => {
+  logger.error('An UNCAUGHT EXCEPTION has been found: ', error);
+  process.exit(1);
+});
+
+(async (): Promise<void> => {
+  const miamiClient: MiamiClient = new MiamiClient();
+
+  await miamiClient.login();
+})();
