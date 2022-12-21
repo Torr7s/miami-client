d92bd5578fc3d83f8467553e126865fe0fc21373
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-20 21:39:09 -0300

    chore: remove functions no longer required

--- src/shared/utils/functions/time.ts
+++ src/shared/utils/functions/time.ts
@@ -1,39 +1,9 @@
 import { time, TimestampStylesString } from 'discord.js';
 
-type Millis = {
-  days?: number;
-  hours?: number;
-  minutes?: number;
-  seconds?: number;
-}
-
-export function convertMs(ms: number): Millis {
-  const seconds: number = ~~(ms / 1000);
-  const minutes: number = ~~(seconds / 60);
-  const hours: number = ~~(minutes / 60);
-  const days: number = ~~(hours / 24);
-
-  return {
-    days,
-    hours: hours % 24,
-    minutes: minutes % 60,
-    seconds: seconds % 60
-  }
-}
-
-export function formatMs(ms: number): string {
-  ms = Math.round(ms / 1000);
-
-  const seconds: number = ms % 60;
-  const minutes: number = ~~((ms / 60) % 60);
-  const hours: number = ~~((ms / 60 / 60) % 24);
-  const days: number = ~~(ms / 60 / 60 / 24);
-
-  return `${days}D ${hours}H ${minutes}m ${seconds}s`;
-}
-
 export function formatTimestamp(date?: string | number | Date, formatter?: TimestampStylesString): string {
-  return (
-    date.toString() === '0' ? '00/00/0000' : time(new Date(date), formatter ?? 'f')
-  );
+  const formattedDate: string = date.toString() === '0'
+    ? '00/00/0000'
+    : time(new Date(date), formatter ?? 'f');
+
+  return formattedDate;
 } 
