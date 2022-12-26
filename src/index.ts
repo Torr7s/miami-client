68db1057178be0aa92e259eea5f75b2f23fb875c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-12-26 15:41:32 -0300

    feat: remove IIFE usage to launch the client

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,9 @@ import { Logger } from './shared/utils/logger';
 
 const logger: Logger = Logger.it('App');
 
-process.on('unhandledRejection', (reason: unknown): void => {
-  logger.error('An UNHANDLED PROMISE has been found: ', reason);
-});
+process.on('unhandledRejection', (reason: unknown): void => logger.error('An UNHANDLED PROMISE has been found: ', reason));
+process.on('uncaughtException', (error: Error): void => logger.error('An UNCAUGHT EXCEPTION has been found: ', error));
 
-process.on('uncaughtException', (error: Error): void => {
-  logger.error('An UNCAUGHT EXCEPTION has been found: ', error);
-  process.exit(1);
-});
+const miamiClient: MiamiClient = new MiamiClient();
 
-(async (): Promise<void> => {
-  const miamiClient: MiamiClient = new MiamiClient();
-
-  await miamiClient.login();
-})();
+miamiClient.login();
