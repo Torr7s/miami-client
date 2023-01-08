1396fee5ef642dc7726db2330ce7acc5445ba8fe
Matheus Torres <77691419+Torr7s@users.noreply.github.com>
2023-01-08 12:43:06 -0300

    feat: rename && return raw variable

--- src/shared/utils/discord/resolvables/permissions.ts
+++ src/shared/utils/discord/resolvables/permissions.ts
@@ -2,13 +2,11 @@ import { PermissionResolvable, PermissionsBitField } from 'discord.js';
 
 import permissionsJson from '../../json/permissions.json';
 
-export const resolvePermissions = (permissions: PermissionResolvable[]): { permissions: string } => {
-  const mappedPermissions: string = new PermissionsBitField(permissions)
+export const resolvePermissions = (permissions: PermissionResolvable[]): string => {
+  const mappedMissingPermissions: string = new PermissionsBitField(permissions)
     .toArray()
     .map((permission: PermissionResolvable): string => `${permissionsJson[String(permission)]}`)
     .join(', ');
 
-  return { 
-    permissions: mappedPermissions 
-  }
+  return mappedMissingPermissions;
 }
